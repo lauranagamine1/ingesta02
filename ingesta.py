7ea4ac78eafcb08392c784e3009b512018a5ab55
Laura Nagamine <166071820+lauranagamine1@users.noreply.github.com>
2025-05-09 17:27:56 -0500

    Update ingesta.py

--- ingesta.py
+++ ingesta.py
@@ -1,9 +1,25 @@
 import boto3
+import pandas as pd
+import pymysql
 
-ficheroUpload = "data.csv"
-nombreBucket = "gcr-output-01"
+nombreBucket = "s6-ingesta02"
+
+conn = pymysql.connect(
+    host='host.docker.internal', 
+    port=3307,
+    user='root',
+    password='1234',
+    database='empleados'
+)
+
+df = pd.read_sql("SELECT * FROM empleados", conn)
+conn.close()
+
+# Guardar como CSV
+csv_file = "data.csv"
+df.to_csv(csv_file, index=False)
 
 s3 = boto3.client('s3')
-response = s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
+response = s3.upload_file(csv_file, nombreBucket, csv_file)
 
-print("Ingesta completada")
+print("Ingesta completada desde MYSQL")
