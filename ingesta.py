e8be78783121872ded526e6c4526be7065604523
Laura Nagamine <166071820+lauranagamine1@users.noreply.github.com>
2025-05-09 20:58:17 -0500

    Update ingesta.py

--- ingesta.py
+++ ingesta.py
@@ -1,25 +1,34 @@
-import boto3
+import os
 import pandas as pd
 import pymysql
+import boto3
 
-nombreBucket = "s6-ingesta02"
-
+# Conexión a MySQL
 conn = pymysql.connect(
-    host='mysql_datos', 
+    host='mysql_datos',     # o la IP interna si es un contenedor
     port=3306,
     user='root',
     password='utec',
     database='empleados'
 )
 
+# Leer tabla
 df = pd.read_sql("SELECT * FROM empleados", conn)
 conn.close()
 
-# Guardar como CSV
+# Guardar CSV
 csv_file = "data.csv"
 df.to_csv(csv_file, index=False)
 
-s3 = boto3.client('s3')
-response = s3.upload_file(csv_file, nombreBucket, csv_file)
+# Subir a S3
+s3 = boto3.client(
+    's3',
+    aws_access_key_id='TU_ACCESS_KEY',
+    aws_secret_access_key='TU_SECRET_KEY',
+    region_name='us-east-1'
+)
 
-print("Ingesta completada desde MYSQL")
+bucket = 's6-ingesta02'
+s3.upload_file(csv_file, bucket, csv_file)
+
+print("Ingesta completada")
